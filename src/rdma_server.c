1d65ea79a996c4ec2909dcacc79670942f4c80b3
Nikolaos Lasithiotakis <nikosl@ics.forth.gr>
2026-05-21 17:04:04 +0000

    Implement graceful shutdown and signal handling for server

--- src/rdma_server.c
+++ src/rdma_server.c
@@ -1,6 +1,8 @@
 #include "utils.h"
 #include <arpa/inet.h>
+#include <poll.h>
 #include <rdma/rdma_cma.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -8,9 +10,20 @@
 #define NUM_WORKERS 4
 #define ALIGNMENT 4096
 
+volatile sig_atomic_t keep_running = 1;
+
+void handle_signal(int sig)
+{
+	if (sig == SIGINT || sig == SIGTERM) {
+		printf("\nShutdown signal received. Initiating clean teardown...\n");
+		keep_running = 0;
+	}
+}
+
 struct server_context {
 	struct ibv_pd *pd;
 	int client_counter;
+	struct rdma_cm_id *listen_id;
 };
 
 struct worker_context {
@@ -29,6 +42,8 @@ struct client_context {
 
 struct server_context server;
 struct worker_context workers[NUM_WORKERS];
+struct client_context *active_clients[MAX_CLIENTS] = { NULL };
+pthread_mutex_t clients_lock = PTHREAD_MUTEX_INITIALIZER;
 
 void *worker_loop(void *arg)
 {
@@ -39,52 +54,59 @@ void *worker_loop(void *arg)
 
 	printf("[Worker %d] Started and waiting for events.\n", worker->worker_id);
 
-	while (1) {
-		if (ibv_get_cq_event(worker->comp_chan, &ev_cq, &ev_ctx)) {
-			perror("ibv_get_cq_event failed");
+	struct pollfd pfd = { .fd = worker->comp_chan->fd, .events = POLLIN };
+
+	while (keep_running) {
+		int ret = poll(&pfd, 1, 500);
+		if (ret == 0)
+			continue;
+		if (ret < 0)
 			break;
-		}
-		ibv_ack_cq_events(ev_cq, 1);
 
-		if (ibv_req_notify_cq(ev_cq, 0)) {
-			perror("ibv_req_notify_cq failed");
-			break;
-		}
+		if (ibv_get_cq_event(worker->comp_chan, &ev_cq, &ev_ctx) == 0) {
+			ibv_ack_cq_events(ev_cq, 1);
+			if (ibv_req_notify_cq(ev_cq, 0))
+				break;
 
-		int num_events;
-		while ((num_events = ibv_poll_cq(worker->cq, 16, wc)) > 0) {
-			for (int i = 0; i < num_events; i++) {
-				struct client_context *client = (struct client_context *)(uintptr_t)wc[i].wr_id;
+			int num_events;
+			while ((num_events = ibv_poll_cq(worker->cq, 16, wc)) > 0) {
+				for (int i = 0; i < num_events; i++) {
+					struct client_context *client = (struct client_context *)(uintptr_t)wc[i].wr_id;
 
-				if (wc[i].status != IBV_WC_SUCCESS) {
-					printf("[Worker %d] Client connection closed or flush error.\n",
-					       worker->worker_id);
-					continue;
-				}
+					if (wc[i].status != IBV_WC_SUCCESS) {
+						printf("[Worker %d] Client connection closed or flush error.\n",
+						       worker->worker_id);
+						continue;
+					}
 
-				if (wc[i].opcode == IBV_WC_RECV) {
-					printf("[Worker %d] Received: %s", worker->worker_id, client->msg);
+					if (wc[i].opcode == IBV_WC_RECV) {
+						printf("[Worker %d] Received: %s", worker->worker_id, client->msg);
 
-					if (strcmp(client->msg, "exit\n") != 0) {
-						struct ibv_sge sge = { .addr = (uintptr_t)client->msg,
-								       .length = MSG_SIZE,
-								       .lkey = client->mr->lkey };
-						struct ibv_recv_wr wr = { .wr_id = (uintptr_t)client,
-									  .sg_list = &sge,
-									  .num_sge = 1 };
-						struct ibv_recv_wr *bad_wr;
-						if (ibv_post_recv(client->cm_id->qp, &wr, &bad_wr)) {
-							perror("ibv_post_recv failed inside worker");
+						if (strcmp(client->msg, "exit\n") != 0) {
+							struct ibv_sge sge = { .addr = (uintptr_t)client->msg,
+									       .length = MSG_SIZE,
+									       .lkey = client->mr->lkey };
+							struct ibv_recv_wr wr = { .wr_id = (uintptr_t)client,
+										  .sg_list = &sge,
+										  .num_sge = 1 };
+							struct ibv_recv_wr *bad_wr;
+							if (ibv_post_recv(client->cm_id->qp, &wr, &bad_wr)) {
+								perror("ibv_post_recv failed inside worker");
+							}
 						}
 					}
 				}
 			}
-		}
-		if (num_events < 0) {
-			fprintf(stderr, "[Worker %d] ibv_poll_cq failed\n", worker->worker_id);
+			if (num_events < 0) {
+				fprintf(stderr, "[Worker %d] ibv_poll_cq failed\n", worker->worker_id);
+				break;
+			}
+		} else {
+			perror("ibv_get_cq_event failed");
 			break;
 		}
 	}
+	printf("[Worker %d] Shutting down...\n", worker->worker_id);
 	return NULL;
 }
 
@@ -208,7 +230,18 @@ void handle_connect_request(struct rdma_cm_id *conn_id)
 	conn_param.retry_count = 7;
 	conn_param.rnr_retry_count = 7;
 
-	if (rdma_accept(conn_id, &conn_param)) {
+	if (rdma_accept(conn_id, &conn_param) == 0) {
+		pthread_mutex_lock(&clients_lock);
+		for (int i = 0; i < MAX_CLIENTS; i++) {
+			if (active_clients[i] == NULL) {
+				active_clients[i] = client;
+				break;
+			}
+		}
+		pthread_mutex_unlock(&clients_lock);
+	} else {
+		free(client->msg);
+		free(client);
 		perror("rdma_accept failed");
 	}
 }
@@ -216,26 +249,95 @@ void handle_connect_request(struct rdma_cm_id *conn_id)
 void connection_manager(struct rdma_event_channel *ec)
 {
 	struct rdma_cm_event *event;
-	while (1) {
-		if (rdma_get_cm_event(ec, &event))
+	struct pollfd pfd = { .fd = ec->fd, .events = POLLIN };
+
+	while (keep_running) {
+		int ret = poll(&pfd, 1, 500);
+		if (ret == 0)
 			continue;
+		if (ret < 0)
+			break;
 
-		int event_type = event->event;
-		struct rdma_cm_id *conn_id = event->id;
-		rdma_ack_cm_event(event);
+		if (rdma_get_cm_event(ec, &event) == 0) {
+			int event_type = event->event;
+			struct rdma_cm_id *conn_id = event->id;
+			rdma_ack_cm_event(event);
 
-		if (event_type == RDMA_CM_EVENT_CONNECT_REQUEST) {
-			handle_connect_request(conn_id);
-		} else if (event_type == RDMA_CM_EVENT_ESTABLISHED) {
-			printf("[CM] New client connected!\n");
-		} else if (event_type == RDMA_CM_EVENT_DISCONNECTED) {
-			printf("[CM] Client disconnected.\n");
+			if (event_type == RDMA_CM_EVENT_CONNECT_REQUEST) {
+				handle_connect_request(conn_id);
+			} else if (event_type == RDMA_CM_EVENT_ESTABLISHED) {
+				printf("[CM] New client connected!\n");
+			} else if (event_type == RDMA_CM_EVENT_DISCONNECTED) {
+				printf("[CM] Client disconnected.\n");
+				pthread_mutex_lock(&clients_lock);
+				for (int i = 0; i < MAX_CLIENTS; i++) {
+					if (active_clients[i] != NULL && active_clients[i]->cm_id == conn_id) {
+						struct client_context *client = active_clients[i];
+
+						if (client->cm_id->qp) {
+							rdma_destroy_qp(client->cm_id);
+						}
+
+						if (client->mr) {
+							ibv_dereg_mr(client->mr);
+						}
+						if (client->msg) {
+							free(client->msg);
+						}
+
+						rdma_destroy_id(client->cm_id);
+
+						free(client);
+						active_clients[i] = NULL;
+
+						printf("[CM] Client slot %d successfully reclaimed.\n", i);
+						break;
+					}
+				}
+				pthread_mutex_unlock(&clients_lock);
+			}
 		}
 	}
 }
 
+void cleanup_server(struct rdma_event_channel *ec)
+{
+	printf("Cleaning up active clients...\n");
+	pthread_mutex_lock(&clients_lock);
+	for (int i = 0; i < MAX_CLIENTS; i++) {
+		if (active_clients[i] != NULL) {
+			struct client_context *client = active_clients[i];
+			rdma_disconnect(client->cm_id);
+			rdma_destroy_qp(client->cm_id);
+			ibv_dereg_mr(client->mr);
+			free(client->msg);
+			rdma_destroy_id(client->cm_id);
+			free(client);
+			active_clients[i] = NULL;
+		}
+	}
+	pthread_mutex_unlock(&clients_lock);
+
+	printf("Waiting for worker threads to exit...\n");
+	for (int i = 0; i < NUM_WORKERS; i++) {
+		pthread_join(workers[i].thread, NULL);
+		ibv_destroy_cq(workers[i].cq);
+		ibv_destroy_comp_channel(workers[i].comp_chan);
+	}
+
+	printf("Destroying global resources...\n");
+	ibv_dealloc_pd(server.pd);
+	rdma_destroy_id(server.listen_id);
+	rdma_destroy_event_channel(ec);
+
+	printf("Shutdown complete.\n");
+}
+
 int main()
 {
+	signal(SIGINT, handle_signal);
+	signal(SIGTERM, handle_signal);
+
 	char ip[64] = { 0 };
 	char port[16] = { 0 };
 	get_config_from_yaml(CONFIG_FILE, ip, sizeof(ip), port, sizeof(port));
@@ -246,12 +348,15 @@ int main()
 		exit(EXIT_FAILURE);
 	}
 
-	struct rdma_cm_id *listen_id = setup_rdma_server(ec, ip, port);
-	setup_worker_pool(listen_id->verbs);
+	server.listen_id = setup_rdma_server(ec, ip, port);
+	setup_worker_pool(server.listen_id->verbs);
 
-	printf("Server listening on %s:%s with %d worker threads\n", ip, port, NUM_WORKERS);
+	printf("Listening on %s:%s with %d worker threads\n", ip, port, NUM_WORKERS);
+	printf("Press Ctrl+C to initiate a clean shutdown.\n");
 
 	connection_manager(ec);
+
+	cleanup_server(ec);
 
 	return 0;
 }
